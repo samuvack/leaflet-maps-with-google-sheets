e95928a120445ab4cbf78fc3e872994f4915c4d6
Samuel Van Ackere <samuel.vanackere@ugent.be>
2020-11-18 22:35:05 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QWxmi77FA4BL5fZ_w7HLoOq5t5tUVeSNGx0cu3oJdqA/edit#gid=0';
+var googleDocURL = 'docs.google.com/spreadsheets/d/1QWxmi77FA4BL5fZ_w7HLoOq5t5tUVeSNGx0cu3oJdqA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
